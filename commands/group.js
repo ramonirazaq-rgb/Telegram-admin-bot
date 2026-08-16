9e58233b143bbe08f459be5b10d776aa651fccc0
patrickrosbin <patrickrosbin@gmail.com>
2026-08-16 01:11:28 +0100

    fixed open and close buttons

--- commands/group.js
+++ commands/group.js
@@ -143,4 +143,80 @@ Only administrators may chat until the group is reopened.`,
 
     });
 
+bot.action("close_group", async (ctx) => {
+
+    try {
+
+        if (!(await canModerate(ctx)))
+            return ctx.answerCbQuery("⛔ Unauthorized.");
+
+        await ctx.telegram.setChatPermissions(
+            ctx.chat.id,
+            {
+                can_send_messages: false,
+                can_send_audios: false,
+                can_send_documents: false,
+                can_send_photos: false,
+                can_send_videos: false,
+                can_send_video_notes: false,
+                can_send_voice_notes: false,
+                can_send_polls: false,
+                can_send_other_messages: false,
+                can_add_web_page_previews: false,
+                can_change_info: false,
+                can_invite_users: false,
+                can_pin_messages: false
+            }
+        );
+
+        await ctx.answerCbQuery("🔒 Group Closed");
+
+    } catch (err) {
+
+        console.error(err);
+
+        await ctx.answerCbQuery("❌ Failed");
+
+    }
+
+});
+
+bot.action("open_group", async (ctx) => {
+
+    try {
+
+        if (!(await canModerate(ctx)))
+            return ctx.answerCbQuery("⛔ Unauthorized.");
+
+        await ctx.telegram.setChatPermissions(
+            ctx.chat.id,
+            {
+                can_send_messages: true,
+                can_send_audios: true,
+                can_send_documents: true,
+                can_send_photos: true,
+                can_send_videos: true,
+                can_send_video_notes: true,
+                can_send_voice_notes: true,
+                can_send_polls: true,
+                can_send_other_messages: true,
+                can_add_web_page_previews: true,
+                can_change_info: false,
+                can_invite_users: true,
+                can_pin_messages: false
+            }
+        );
+
+        await ctx.answerCbQuery("🔓 Group Opened");
+
+    } catch (err) {
+
+        console.error(err);
+
+        await ctx.answerCbQuery("❌ Failed");
+
+    }
+
+});
+
 };
